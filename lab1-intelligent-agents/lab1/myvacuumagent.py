106670b1c7191f026a5beacb31db499f19341e1a
abbasalubeid <abbasalubeid1@gmail.com>
2024-10-04 11:15:15 +0200

    Task 2 done

--- lab1-intelligent-agents/lab1/myvacuumagent.py
+++ lab1-intelligent-agents/lab1/myvacuumagent.py
@@ -1,4 +1,5 @@
 from lab1.liuvacuum import *
+from collections import deque
 
 DEBUG_OPT_DENSEWORLDMAP = False
 
@@ -47,7 +48,10 @@ class MyAgentState:
         self.max_y = height - 2
 
         self.start = True
-        self.goHome = False
+        self.go_home = False
+
+        self.queue = deque() # For the new neighbours of the current position
+        self.backtrack_stack = [] # For stacking a path to a certain cell in the map
 
     """
     Update perceived agent location
@@ -97,7 +101,7 @@ class MyVacuumAgent(Agent):
     def __init__(self, world_width, world_height, log):
         super().__init__(self.execute)
         self.initial_random_actions = 10
-        self.iteration_counter = 1000
+        self.iteration_counter = 100000
         self.state = MyAgentState(world_width, world_height)
         self.log = log
 
@@ -180,46 +184,45 @@ class MyVacuumAgent(Agent):
         # Debug
         self.state.print_world_debug()
 
-        def turnLeft():
+        def turn_left():
             self.log("turning left")
             self.state.direction = (self.state.direction + 3) % 4
             self.state.last_action = ACTION_TURN_LEFT
             return ACTION_TURN_LEFT
 
-        def turnRight():
+        def turn_right():
             self.log("turning right")
             self.state.direction = (self.state.direction + 1) % 4
             self.state.last_action = ACTION_TURN_RIGHT
             return ACTION_TURN_RIGHT
 
-        def moveForward():
+        def move_forward():
             self.log("moving forward")
             self.state.last_action = ACTION_FORWARD
             return ACTION_FORWARD
 
-        def goToStartPos():
-
+        def go_to_start_pos():
             if self.state.direction != AGENT_DIRECTION_NORTH and self.state.pos_y > 1:
-                return turnRight()
+                return turn_right()
             elif home and self.state.direction != AGENT_DIRECTION_EAST:
-                return turnRight()
+                return turn_right()
             elif bump:
-                return turnLeft()
+                return turn_left()
             elif (self.state.world[self.state.pos_x - 1][self.state.pos_y] == AGENT_STATE_HOME) and not(self.state.direction == AGENT_DIRECTION_EAST):
-                return turnRight()
+                return turn_right()
             elif (self.state.world[self.state.pos_x - 1][self.state.pos_y] == AGENT_STATE_HOME) and (self.state.direction == AGENT_DIRECTION_EAST):
                 self.state.start = False
                 # Do nothing
                 self.state.last_action = ACTION_SUCK
                 return ACTION_SUCK
             else:
-                return moveForward()
+                return move_forward()
 
-        def boundaryReached():
+        def boundary_reached():
 
             if self.state.min_x > self.state.max_x and self.state.min_y > self.state.max_y:
                 self.log("Going Home")
-                self.state.goHome = True
+                self.state.go_home = True
                 return False
 
             if self.state.direction == AGENT_DIRECTION_EAST and self.state.pos_x == self.state.max_x:
@@ -236,27 +239,234 @@ class MyVacuumAgent(Agent):
                 return True
             return False
 
-        def goHome():
+        def go_home():
 
             if self.state.direction  != AGENT_DIRECTION_NORTH and self.state.pos_y > 1:
-                return turnRight()
+                return turn_right()
             if home:
                 self.log("Home and done!")
                 self.iteration_counter = 0
                 return ACTION_NOP
             elif bump:
-                return turnLeft()
+                return turn_left()
             else:
-                return moveForward()
+                return move_forward()
 
+        def update_queue():
+            updated = False
+            current_pos = (self.state.pos_x, self.state.pos_y)
+
+            # Add the cell above if it's unknown/home and not already in the queue
+            if self.state.world[self.state.pos_x][self.state.pos_y - 1] == AGENT_STATE_UNKNOWN or self.state.world[self.state.pos_x][self.state.pos_y - 1] == AGENT_STATE_HOME:
+                new_pos = (self.state.pos_x, self.state.pos_y - 1)
+                if new_pos not in [pos[0] for pos in self.state.queue]:  # Check if not in queue by comparing first elements (second is parent)
+                    updated = True
+                    self.state.queue.append((new_pos, current_pos))
+
+            # Add the cell below if it's unknown/home and not already in the queue
+            if self.state.world[self.state.pos_x][self.state.pos_y + 1] == AGENT_STATE_UNKNOWN or self.state.world[self.state.pos_x][self.state.pos_y + 1] == AGENT_STATE_HOME:
+                new_pos = (self.state.pos_x, self.state.pos_y + 1)
+                if new_pos not in [pos[0] for pos in self.state.queue]:
+                    updated = True
+                    self.state.queue.append((new_pos, current_pos))
+
+            # Add the cell to the left if it's unknown/home and not already in the queue
+            if self.state.world[self.state.pos_x - 1][self.state.pos_y] == AGENT_STATE_UNKNOWN or self.state.world[self.state.pos_x - 1][self.state.pos_y] == AGENT_STATE_HOME:
+                new_pos = (self.state.pos_x - 1, self.state.pos_y)
+                if new_pos not in [pos[0] for pos in self.state.queue]:
+                    updated = True
+                    self.state.queue.append((new_pos, current_pos))
+
+            # Add the cell to the right if it's unknown/home and not already in the queue
+            if self.state.world[self.state.pos_x + 1][self.state.pos_y] == AGENT_STATE_UNKNOWN or self.state.world[self.state.pos_x + 1][self.state.pos_y] == AGENT_STATE_HOME:
+                new_pos = (self.state.pos_x + 1, self.state.pos_y)
+                if new_pos not in [pos[0] for pos in self.state.queue]:
+                    updated = True
+                    self.state.queue.append((new_pos, current_pos))
+
+            if updated:
+                print("Queue updated with unknown cells: ", self.state.queue)
+
+        def calculate_path(target):
+            """
+            Uses BFS to find and return path from the current position to a certain target cell
+            """
+            start_pos = (self.state.pos_x, self.state.pos_y)
+
+            queue = deque()
+            queue.append(start_pos)
+            visited = set()
+            visited.add(start_pos)
+            came_from = {start_pos: None}
+
+            while queue:
+                current = queue.popleft()
+                if current == target:
+                    # Goal reached, reconstruct the path
+                    path = []
+                    while current is not None:
+                        path.append(current)
+                        current = came_from[current]
+                    return path  # Return the path as a list
+
+                x, y = current
+
+                # Explore neighbors individually (left, right, up, down)
+
+                # Left neighbor
+                neighbor_x = x - 1
+                neighbor_y = y
+                if (self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_CLEAR or
+                    self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_HOME) and (neighbor_x, neighbor_y) not in visited:
+                    visited.add((neighbor_x, neighbor_y))
+                    came_from[(neighbor_x, neighbor_y)] = current
+                    queue.append((neighbor_x, neighbor_y))
+
+                # Right neighbor
+                neighbor_x = x + 1
+                neighbor_y = y
+                if (self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_CLEAR or
+                    self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_HOME) and (neighbor_x, neighbor_y) not in visited:
+                    visited.add((neighbor_x, neighbor_y))
+                    came_from[(neighbor_x, neighbor_y)] = current
+                    queue.append((neighbor_x, neighbor_y))
+
+                # Above neighbor
+                neighbor_x = x
+                neighbor_y = y - 1
+                if (self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_CLEAR or
+                    self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_HOME) and (neighbor_x, neighbor_y) not in visited:
+                    visited.add((neighbor_x, neighbor_y))
+                    came_from[(neighbor_x, neighbor_y)] = current
+                    queue.append((neighbor_x, neighbor_y))
+
+                # Under neighbor
+                neighbor_x = x
+                neighbor_y = y + 1
+                if (self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_CLEAR or
+                    self.state.world[neighbor_x][neighbor_y] == AGENT_STATE_HOME) and (neighbor_x, neighbor_y) not in visited:
+                    visited.add((neighbor_x, neighbor_y))
+                    came_from[(neighbor_x, neighbor_y)] = current
+                    queue.append((neighbor_x, neighbor_y))
+
+            # No path found
+            return None
+
+        def directly_reachable(target_x, target_y):
+            """
+            Returns true if the target cell is reachable from the current cell
+            """
+            if target_x == self.state.pos_x and target_y == self.state.pos_y:
+                return True
+
+            # Target is directly to the left of the current position
+            if target_x == self.state.pos_x - 1 and target_y == self.state.pos_y:
+                return True
+
+            # Target is directly to the right of the current position
+            if target_x == self.state.pos_x + 1 and target_y == self.state.pos_y:
+                return True
+
+            # Target is directly above the current position
+            if target_y == self.state.pos_y - 1 and target_x == self.state.pos_x:
+                return True
+
+            # Target is directly below the current position
+            if target_y == self.state.pos_y + 1 and target_x == self.state.pos_x:
+                return True
+
+            return False
+
+        def next_move():
+
+            if home and self.state.go_home:
+                self.log("Home and done!")
+                self.iteration_counter = 0
+                return ACTION_NOP
+
+            update_queue()
+
+            if not self.state.queue:
+                self.log("The whole map is updated, lets go home")
+                print("Add home to queue!")
+                self.state.queue.append(((1,1), (1, 1)))
+                self.state.go_home = True
+
+            print("Queue :", self.state.queue)
+
+            # If stack is not empty, we are on a path to a certain target
+            if self.state.backtrack_stack:
+                print("Stack not empty: ", self.state.backtrack_stack)
+                target_x, target_y = self.state.backtrack_stack[-1]
+            else:
+                ((target_x, target_y), parent) = self.state.queue[0]
+
+                if not directly_reachable(target_x, target_y) and not self.state.backtrack_stack:
+                    print("Target pos not reachable: ", target_x, target_y, " looking for path...")
+                    path = calculate_path(parent)
+                    if path:
+                        print("A path to the target: ", path)
+                        self.state.backtrack_stack.extend(path)
+                        target_x, target_y = self.state.backtrack_stack[-1]
+                        print("new stack with path to: ",parent, self.state.backtrack_stack)
+
+                    elif not path and (target_x, target_y) == (1, 1):
+                        print("No path to home...")
+                        self.iteration_counter = 0
+                        return ACTION_NOP
+
+
+            print("Current Position: ({}, {}) Direction: {} Target: ({}, {})".format(
+                self.state.pos_x, self.state.pos_y, self.state.direction, target_x, target_y))
+
+
+            if target_x == self.state.pos_x and target_y == self.state.pos_y:
+                # if the target was from the stack, pop the stack to move to next target in the next call
+                if self.state.backtrack_stack:
+                    self.state.backtrack_stack.pop()
+                    print("Backtrack stack after pop: ", self.state.backtrack_stack)
+                elif self.state.queue:
+                    self.state.queue.popleft()
+                    print("Queue after left pop: ", self.state.queue)
+                self.state.last_action = ACTION_SUCK
+                return ACTION_SUCK  # Do nothing
+            elif target_x < self.state.pos_x and self.state.direction != AGENT_DIRECTION_WEST:
+                return turn_right()
+            elif target_x > self.state.pos_x and self.state.direction != AGENT_DIRECTION_EAST:
+                return turn_right()
+            elif target_y < self.state.pos_y and self.state.direction != AGENT_DIRECTION_NORTH:
+                return turn_right()
+            elif target_y > self.state.pos_y and self.state.direction != AGENT_DIRECTION_SOUTH:
+                return turn_right()
+            else:
+                # if the target was from the stack, pop the stack to move to next target in the next call
+                if self.state.backtrack_stack:
+                    self.state.backtrack_stack.pop()
+                    print("Backtrack stack after pop: ", self.state.backtrack_stack)
+                else:
+                    self.state.queue.popleft()
+                    print("Queue after left pop: ", self.state.queue)
+
+                print("Moving forward towards: ", target_x, target_y)
+                return move_forward()
+
+
+        # Task2
         if dirt:
             self.state.last_action = ACTION_SUCK
             return ACTION_SUCK
-        elif self.state.start:
-            return goToStartPos()
-        elif boundaryReached():
-            return turnRight()
-        elif self.state.goHome:
-            return goHome()
         else:
-            return moveForward()
+            return next_move()
+
+        # Task1
+        # if dirt:
+        #     self.state.last_action = ACTION_SUCK
+        #     return ACTION_SUCK
+        # elif self.state.start:
+        #     return go_to_start_pos()
+        # elif boundary_reached():
+        #     return turn_right()
+        # elif self.state.go_home:
+        #     return go_home()
+        # else:
+        #     return move_forward()
