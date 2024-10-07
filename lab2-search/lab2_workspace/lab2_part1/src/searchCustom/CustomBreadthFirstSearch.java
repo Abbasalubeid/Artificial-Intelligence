25d69444256536acca6bcce5391ab220a03066b7
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-10-07 20:46:44 +0200

    FIFO

--- lab2-search/lab2_workspace/lab2_part1/src/searchCustom/CustomBreadthFirstSearch.java
+++ lab2-search/lab2_workspace/lab2_part1/src/searchCustom/CustomBreadthFirstSearch.java
@@ -4,6 +4,6 @@ package searchCustom;
 public class CustomBreadthFirstSearch  extends CustomGraphSearch{
 
 	public CustomBreadthFirstSearch(int maxDepth) {
-	    super(false); // Use false to add to the back, mimicking queue behavior
+	    super(false); // Use false to add to the back, (FIFO)
 	}
 };
